3eb6d1ca0222ba8f3e74df37232b1bc98df4cd3c
Tony Kalalian <tkalalian@gmail.com>
2023-04-22 15:07:32 +0300

    Added work section

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-var items = [
-  "Go to gym",
-  "Do some code",
-  "Drink a coffee",
-  "Work on my projects",
-];
+let items = ["Go to gym", "Drink a coffee", "Work on my projects"];
+let workItems = [];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-port = 3000;
+port = 3200;
 
 app.get("/", (req, res) => {
   let today = new Date();
-
-  var options = {
+  let options = {
     weekday: "long",
     year: "numeric",
     month: "long",
@@ -24,13 +19,22 @@ app.get("/", (req, res) => {
   };
 
   let day = today.toLocaleDateString("en-US", options);
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 app.post("/", (req, res) => {
-  item = req.body.username;
+  let item = req.body.newItem;
   items.push(item);
+  console.log(req.body);
   res.redirect("/");
 });
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work day", newListItems: workItems });
+});
+app.post("/work", (req, res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
 app.listen(port, () => {
-  console.log("server is running on port 3000");
+  console.log(`server is running on port ${port}`);
 });
