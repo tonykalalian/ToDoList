7d5fa8441eb5e5d7e3d35cc35ed131807bc89bbb
Tony Kalalian <tkalalian@gmail.com>
2023-04-22 16:10:23 +0300

    my last update i think

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 const app = express();
-let items = ["Go to gym", "Drink a coffee", "Work on my projects"];
-let workItems = [];
+const items = ["Go to gym", "Drink a coffee", "Work on my projects"];
+const workItems = [];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,19 +12,11 @@ app.use(express.static("public"));
 port = 3200;
 
 app.get("/", (req, res) => {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  const day = date.getDate();
   res.render("list", { listTitle: day, newListItems: items });
 });
 app.post("/", (req, res) => {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   if (req.body.list === "Work day") {
     workItems.push(item);
     res.redirect("/work");
@@ -35,7 +29,7 @@ app.get("/work", (req, res) => {
   res.render("list", { listTitle: "Work day", newListItems: workItems });
 });
 app.post("/work", (req, res) => {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   workItems.push(item);
   console.log(req.body);
   res.redirect("/work");
---
+++ date.js
@@ -0,0 +1,22 @@
+//ES6
+exports.getDate = () => {
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return today.toLocaleDateString("en-US", options);
+};
+exports.getDay = () => {
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+  };
+
+  return today.toLocaleDateString("en-US", options);
+};
